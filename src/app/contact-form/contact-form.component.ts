42efff4b1a30356cf24d83001ae206212eebda20
SZBM7Z@ALLYIPOC.COM <SZBM7Z@ALLYIPOC.COM>
2019-07-24 15:05:50 -0400

    contact form in progress

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,104 @@
-import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
+import { NgForm } from '@angular/forms';
+
+import { DataService } from '../data.service'
+import { fadeInAnimation } from '../animations/fade-in.animation';
+
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  styleUrls: ['./contact-form.component.css'],
+  animations: [fadeInAnimation]
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  contactForm: NgForm;
+  @ViewChild('contactForm') currentForm: NgForm;
 
-  ngOnInit() {
+  successMessage: string;
+  errorMessage: string;
+
+  contact: object;
+
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  getRecordForEdit(){
+    this.route.params
+      .switchMap((params: Params) => this.dataService.getRecord("contact", +params['id']))
+      .subscribe(contact => this.contact = contact);
   }
 
+  ngOnInit() {
+    this.route.params
+      .subscribe((params: Params) => {
+        (+params['id']) ? this.getRecordForEdit() : null;
+      });
+  }
+
+  saveContact(contactForm: NgForm){
+    if(typeof contactForm.value.id === "number"){
+      this.dataService.editRecord("contact", contactForm.value, contactForm.value.id)
+          .subscribe(
+            company => this.successMessage = "Record updated successfully",
+            error =>  this.errorMessage = <any>error);
+    }else{
+      this.dataService.addRecord("contact", contactForm.value)
+          .subscribe(
+            contact => this.successMessage = "Record added successfully",
+            error =>  this.errorMessage = <any>error);
+            this.contact = {};
+            this.contactForm.reset()
+    }
+
+  }
+
+  ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    this.contactForm = this.currentForm;
+    this.contactForm.valueChanges
+      .subscribe(
+        data => this.onValueChanged(data)
+      );
+  }
+
+  
+  onValueChanged(data?: any) {
+    let form = this.contactForm.form;
+
+    for (let field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  formErrors = {
+    'first_name': ''
+  };
+
+  validationMessages = {
+    'first_name': {
+      'required': 'First name is required.',
+      'minlength': 'First name must be at least 2 characters long.',
+      'maxlength': 'First name cannot be more than 30 characters long.'
+    }
+  };
 }
