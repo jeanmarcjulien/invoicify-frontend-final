13e46f809cce09a0b1f5d8485ff577da2b3159e1
Justin Farkas <farkas_justin@yahoo.com>
2019-07-24 14:24:35 -0400

    fixing some animation stuff

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInAnimation } from 'app/animations/fade-in.animation';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
+  animations: [fadeInAnimation]
 })
 export class ContactComponent implements OnInit {
 
