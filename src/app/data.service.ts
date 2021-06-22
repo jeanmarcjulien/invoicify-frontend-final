fbaa9874b68fe7f9bdadbe12cb1553264c7efa2d
jeanmarcjulien <julienjeanmarc@gmail.com>
2021-06-22 03:02:37 -0500

    initial commit

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -11,7 +11,8 @@ import 'rxjs/add/observable/empty';
 @Injectable()
 export class DataService {
 
-    private baseUrl = 'https://morning-hollows-64649.herokuapp.com/api/'
+    private baseUrl = 'http://localhost:8080/api/'
+
     found = false;
 
     options = new RequestOptions({ withCredentials: true });
